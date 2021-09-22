8372de4f7f64a9199072c03081d216d2a9ea4e41
Jessica Fatta <jessicafatta@gmail.com>
2021-09-21 22:36:38 -0400

    added car

---
+++ object-examples.js
@@ -0,0 +1,10 @@
+var car = {
+  make: 'Subaru',
+  model: 'Forester',
+  year: '2015',
+  trim: 'Limited',
+  color: 'champange',
+  odometer: 'a lot',
+  crossCountryTrips: 'yes',
+  boughtNew: 'no',
+}
